1a30a0a97172cf450ff94da599c66dbfa40dab1b
Daniyal842 <developerdaniyal98@gmail.com>
2025-02-28 19:17:59 +0500

    add

---
+++ lib/splashscreen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:project3/loginscreen.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>Loginscreen()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/atm3.png',height: 400,),
+      ),
+    );
+  }
+}
